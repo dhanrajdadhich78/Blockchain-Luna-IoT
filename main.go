b14d35becff6064702f471b1803ce951128e8cda
Dhanraj Dadhich <dhanraj.dadhich78@gmail.com>
2018-01-19 10:29:56 +0200

    add block

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+	"strconv"
+	"bytes"
+	"crypto/sha256"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
