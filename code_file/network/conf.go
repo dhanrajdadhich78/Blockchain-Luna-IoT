62f338910a8afc5e011fe83d3e303cfbc564295e
Dhanraj Dadhich <dhanraj.dadhich78@gmail.com>
2018-02-22 11:03:45 +0200

    remove unused files

--- code_file/network/conf.go
+++
@@ -1,9 +0,0 @@
-package network
-
-import "flag"
-
-var (
-	apiAddr   = flag.String("api", ":3001", "HTTP server address for API")
-	p2pAddr   = flag.String("p2p", ":6001", "WebSocket server address for P2P")
-	p2pOrigin = flag.String("origin", "http://127.0.0.1", "P2P origin")
-)
