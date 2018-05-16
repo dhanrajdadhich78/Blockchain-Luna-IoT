a5dbf5aa60cc77ed40e88ef28a055168d5023be4
Dhanraj Dadhich <dhanraj.dadhich78@gmail.com>
2018-05-16 17:34:00 +0300

    add digest register to raft
    (cherry picked from commit b67f77ae2a30006f3154b12857ba77bb7b8b7aa3)

---
+++ raft/reg_digest.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func regDigest() {
+	url := "http://" + os.Getenv("DIGEST_NODE") + ":8888/hello/raft"
+	values := map[string]string{
+		"Address":   os.Getenv("USER_ADDRESS"),
+		"PrivKey":   os.Getenv("USER_PRIVKEY"),
+		"Pubkey":    os.Getenv("USER_PUBKEY"),
+		"AES":       os.Getenv("PASSWORD"),
+		"Url":       "http://" + os.Getenv("PUBLIC_IP") + ":11000/",
+		"ServerKey": os.Getenv("SERVER_KEY"),
+	}
+
+	jsonValue, _ := json.Marshal(values)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+}
